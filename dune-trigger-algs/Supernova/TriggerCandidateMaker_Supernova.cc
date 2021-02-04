0fd5a5713ce4bd5503cb12dffe5182fd745a93cd
Thiago Bezerra <thiagojcb@gmail.com>
2021-02-04 10:48:56 -0600

    fixing missing typos from last update on TA addition

--- dune-trigger-algs/Supernova/TriggerCandidateMaker_Supernova.cc
+++ dune-trigger-algs/Supernova/TriggerCandidateMaker_Supernova.cc
@@ -10,26 +10,26 @@ using pd_clock = std::chrono::duration<double, std::ratio<1, 50'000'000>>;
 void TriggerCandidateMakerSupernova::operator()(const TriggerActivity& activity,
                                                std::vector<TriggerCandidate>& cand) {
   int64_t time = activity.time_start;
-  FlushOldActivity(time); // get rid of old clusters in the buffer
+  FlushOldActivity(time); // get rid of old activities in the buffer
   if (activity.tp_list.size()>m_hit_threshold)
-    m_cluster.push_back(activity);
+    m_activity.push_back(activity);
 
   // Yay! we have a trigger!
-  if (m_cluster.size() > m_threshold) {
+  if (m_activity.size() > m_threshold) {
     std::chrono::time_point<std::chrono::steady_clock> now;
 
     // set all bits to one (probably this will mean down the line: "record every part of the detector")
     uint32_t detid = 0xFFFFFFFF;
     
-    TriggerCandidate trigger {time - 500'000'000, // time_start (10 seconds before the start of the cluster)
+    TriggerCandidate trigger {time - 500'000'000, // time_start (10 seconds before the start of the activity)
                              activity.time_end, // time_end, but that should probably be _at least_ this number
                              int64_t(pd_clock(now.time_since_epoch()).count()), // this is now in dune time, with a cast to avoid narrowing warning
                              detid, // all the detector
 	                     0, //type ( flag that says what type of trigger might be (e.g. SN/Muon/Beam) )
 	                     0, //algorithm ( flag that says which algorithm created the trigger (e.g. SN/HE/Solar) )
 	                     0, //version of the above
-                             m_cluster}; // TAs used to form this trigger candidate
-    m_cluster.clear();
+                             m_activity}; // TAs used to form this trigger candidate
+    m_activity.clear();
     // Give the trigger word back
     cand.push_back(trigger);
     return;
--- dune-trigger-algs/Supernova/TriggerCandidateMaker_Supernova.hh
+++ dune-trigger-algs/Supernova/TriggerCandidateMaker_Supernova.hh
@@ -7,24 +7,24 @@
 
 namespace DuneTriggerAlgs {
   class TriggerCandidateMakerSupernova: public TriggerCandidateMaker {
-    /// This decision maker just counts the number of clusters in the time_window and triggers
-    /// if the number of clusters exceeds that.
+    /// This decision maker just counts the number of activities in the time_window and triggers
+    /// if the number of activities exceeds that.
     
   public:
-    /// The function that gets call when there is a new cluster
+    /// The function that gets call when there is a new activity
     void operator()(const TriggerActivity&, std::vector<TriggerCandidate>&);
     
   protected:
-    std::vector<TriggerActivity> m_cluster;
+    std::vector<TriggerActivity> m_activity;
     std::atomic<int64_t > m_time_window   = {500'000'000};
     std::atomic<uint16_t> m_threshold     = {6};
     std::atomic<uint16_t> m_hit_threshold = {3};
 
 
-    /// this function gets rid of the old clusters
+    /// this function gets rid of the old activities
     void FlushOldActivity(int64_t time_now) {
       int64_t how_far = time_now - m_time_window;
-      std::remove_if(m_cluster.begin(), m_cluster.end(),
+      std::remove_if(m_activity.begin(), m_activity.end(),
                      [how_far, this] (auto& c) -> bool {
                        return (c.time_start<how_far);
                      });
